d38478a29ead93e0d0eacf420976bebcc1fe5ec6
Elias Kibret <eliaskibret2013n2@gmail.com>
2024-06-28 20:28:54 -0500

    comment

--- 00041-First-Missing-Positive/0041-First-Missing-Positive.py
+++ 00041-First-Missing-Positive/0041-First-Missing-Positive.py
@@ -1,8 +1,8 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
         #I will use Hashmap , Store every value ,
-        # The find max for the number , I will iterate through starting from 1 to the maximum
-        # the if there is a missing number in hashmap I will return that number
+        # Then find max for the number , I will iterate through starting from 1 to the maximum
+        # then if there is a missing number in hashmap I will return that number
         # else I will return max number plus 1 which is the will be the next postive number
         # one edge I have to make sure that the max number is postive number 
         mp={} 
