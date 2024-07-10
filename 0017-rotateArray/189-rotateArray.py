29ce9b780d35c4765e3a05e709d168dc7cce51c8
Elias Kibret <eliaskibret2013n2@gmail.com>
2024-07-10 18:37:49 -0500

    0189-rotate-array

---
+++ 0017-rotateArray/189-rotateArray.py
@@ -0,0 +1,22 @@
+from typing import List
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k = k % n  # In case k is greater than the length of nums
+        
+        def reverse(start: int, end: int) -> None:
+            while start < end:
+                nums[start], nums[end] = nums[end], nums[start]
+                start += 1
+                end -= 1
+        
+      
+        reverse(0, n - 1)
+     
+        reverse(0, k - 1)
+        
+        reverse(k, n - 1)
