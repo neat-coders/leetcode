f7c67d32bb78007c6c8d32655e94909d12d7deeb
Abel H <134791272+abDev2008@users.noreply.github.com>
2024-07-31 08:28:59 -0500

    Create SetMatrixZeros_73.java

---
+++ 0028-SetMatrixZeroes/SetMatrixZeros_73.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+int m = matrix.length;
+        int n = matrix[0].length;
+        boolean[] rowZero = new boolean[m];
+        boolean[] colZero = new boolean[n];
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    rowZero[i] = true;
+                    colZero[j] = true;
+                }
+            }
+        }
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (rowZero[i] || colZero[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
